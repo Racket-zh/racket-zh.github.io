f3b31e15dd2634c74432dc4a085dea41da57a106
syntacticlosure <1227847322@qq.com>
2018-06-13 13:35:37 +0800

    change the configuration

--- frog.rkt
+++ frog.rkt
@@ -4,7 +4,7 @@
 ;; in frog/params.
 (define/contract (init)
   (-> any)
-  (current-scheme/host "http://www.example.com")
+  (current-scheme/host "http://www.racket-zh.org")
   (current-title "Racket中文网站")
   (current-author "The Unknown Author"))
 
