3ad7319ae9b12ea0e75311f4d3cf7d25e5991274
syntacticlosure <1227847322@qq.com>
2018-06-17 13:32:39 +0800

    change the style

--- frog.rkt
+++ frog.rkt
@@ -6,7 +6,22 @@
   (-> any)
   (current-scheme/host "http://www.racket-zh.org")
   (current-title "Racket中文网站")
-  (current-author "未知作者"))
+  (current-uri-prefix "/")
+  (current-author "The Unknown Author")
+  (current-editor "$EDITOR")
+  (current-editor-command "{editor} {filename}")
+  (current-show-tag-counts? #t)
+  (current-permalink "/{year}/{month}/{filename}.html")
+  (current-index-full? #f)
+  (current-feed-full? #t)
+  (current-max-feed-items 20)
+  (current-decorate-feed-uris? #t)
+  (current-feed-image-bugs? #t)
+  (current-posts-per-page 10)
+  (current-index-newest-first? #t)
+  (current-posts-index-uri "/index.html")
+  (current-source-dir "_src")
+  (current-output-dir "."))
 
 ;; Called once per post and non-post page, on the contents.
 (define/contract (enhance-body xs)
@@ -15,10 +30,10 @@
   (~> xs
       (syntax-highlight #:python-executable (if (eq? (system-type 'os) 'windows) "python.exe"
                                                 "python")
-                        #:line-numbers? #t
-                        #:css-class "source")
+                        #:line-numbers? #f
+                        #:css-class "pygments")
       (auto-embed-tweets #:parents? #t)
-      (add-racket-doc-links #:code? #t #:prose? #f)))
+      (add-racket-doc-links #:code? #t #:prose? #t)))
 
 ;; Called from `raco frog --clean`.
 (define/contract (clean)
